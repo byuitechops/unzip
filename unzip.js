ecb20727a8fa6b9d6cb4f4be3fff118d506b14d0
Danverde <gre12014@buyi.edu>
2018-02-27 10:46:08 -0700

    fixed fatal Err handling

--- unzip.js
+++ unzip.js
@@ -1,22 +1,20 @@
-/*eslint-env node, es6*/
-/*eslint no-unused-vars: 1*/
 
 const decompress = require('decompress');
 
 module.exports = (course, stepCallback) => {
-  try {
+    try {
     /* Unzip the course into a new folder */
-    decompress(course.info.originalZipPath, course.info.unzippedPath)
-      .then((files) => {
-        course.message('Course successfully unzipped');
-        stepCallback(null, course);
-      }, (promiseError) => {
-        course.fatalError(promiseError);
-        stepCallback(promiseError, course);
-      });
+        decompress(course.info.originalZipPath, course.info.unzippedPath)
+            .then((files) => {
+                course.message('Course successfully unzipped');
+                stepCallback(null, course);
+            }, (promiseError) => {
+                course.fatalError(promiseError);
+                stepCallback(promiseError, course);
+            });
 
-  } catch (e) {
-    course.fataError(e);
-    stepCallback(e, course);
-  }
+    } catch (e) {
+        course.fatalError(e);
+        stepCallback(e, course);
+    }
 };
