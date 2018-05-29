37e2b4bb83672b1a6d2c3feb8307a9923cf325b6
Danverde <gre12014@byui.edu>
2018-05-29 10:15:25 -0600

    removed redundant err handling

--- unzip.js
+++ unzip.js
@@ -1,19 +1,13 @@
 const decompress = require('decompress');
 
 module.exports = (course, stepCallback) => {
-    try {
-        /* Unzip the course into a new folder */
-        decompress(course.info.originalZipPath, course.info.unzippedPath)
-            .then((files) => {
-                course.message('Course successfully unzipped');
-                stepCallback(null, course);
-            }, (promiseError) => {
-                course.fatalError(promiseError);
-                stepCallback(promiseError, course);
-            }).catch(course.error);
-
-    } catch (e) {
-        course.fatalError(e);
-        stepCallback(e, course);
-    }
+    /* Unzip the course into a new folder */
+    decompress(course.info.originalZipPath, course.info.unzippedPath)
+        .then((files) => {
+            course.message('Course successfully unzipped');
+            stepCallback(null, course);
+        }, (promiseError) => {
+            course.fatalError(promiseError);
+            stepCallback(promiseError, course);
+        });
 };
